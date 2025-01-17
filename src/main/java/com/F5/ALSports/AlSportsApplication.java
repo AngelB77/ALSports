f02f1fd8b3df7000cf967b6d3cc1f88766c2b259
lltosso <luis.tosso@gmail.com>
2025-01-17 13:58:58 +0100

    first commit

---
+++ src/main/java/com/F5/ALSports/AlSportsApplication.java
@@ -0,0 +1,13 @@
+package com.F5.ALSports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlSportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlSportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/F5/ALSports/AlSportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.F5.ALSports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlSportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
