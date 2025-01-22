e680bedf37aa5b514534147b7c893daa39a4b68c
lltosso <luis.tosso@gmail.com>
2025-01-22 11:59:16 +0100

    agregado update, en proceso DTO

--- src/main/java/com/F5/ALSports/controller/ProductController.java
+++ src/main/java/com/F5/ALSports/controller/ProductController.java
@@ -3,9 +3,12 @@ package com.F5.ALSports.controller;
 
 import com.F5.ALSports.model.Product;
 import com.F5.ALSports.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ProductController {
@@ -25,8 +28,33 @@ public class ProductController {
     public void creteProduct(@RequestBody Product newProduct) {
         productService.addProduct(newProduct);
     }
+
     @DeleteMapping("/products/{id}")
     public void deleteProductById(@PathVariable int id) {
         productService.deleteProduct(id);
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Product> findProductById(@PathVariable int id) {
+        Optional<Product> foundProduct = productService.findProduct(id);
+
+        if(foundProduct.isPresent()) {
+            return new ResponseEntity<>(foundProduct.get(), HttpStatus.FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable int id, @RequestBody  Product
+            updatedProduct) {
+
+        try {
+            //actualizar los campos del product en el caso de que encuentre
+            Product product = productService.updatedProduct(id, updatedProduct);
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } catch (Exception e) {
+            //En el caso de que no se encuentre devuelve no found
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        }
     }
 }
--- src/main/java/com/F5/ALSports/service/ProductService.java
+++ src/main/java/com/F5/ALSports/service/ProductService.java
@@ -5,6 +5,7 @@ import com.F5.ALSports.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -20,11 +21,34 @@ public class ProductService {
     }
 
     public Product addProduct(Product newProduct){
+
         return productRepository.save(newProduct);
     }
+
     public void deleteProduct(int id) {
         productRepository.deleteById(id);
     }
 
 
+    public Optional<Product> findProduct(int id) {
+        return productRepository.findById(id);
+    }
+
+    public Product updatedProduct(int id, Product updatedProduct) {
+        //Buscar producto por id
+        Optional<Product> foundProduct = productRepository.findById(id);
+
+        if(foundProduct.isPresent()) {
+            Product existingProduct = foundProduct.get();
+
+            //Actualizar los campos
+            existingProduct.setName(updatedProduct.getName());
+
+            //Guarda el producto
+            return productRepository.save(existingProduct);
+
+        }
+        //Enviar mensaje al usuario
+        throw new RuntimeException("Product not found with id: " + id);
+    }
 }
