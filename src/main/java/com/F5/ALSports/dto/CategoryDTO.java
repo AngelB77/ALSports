35ab79e9debc3ce00212ad5c625127b703c545fd
AngelB77 <AngelB1977@gamil.com>
2025-01-23 14:11:03 +0100

    dto_min11

---
+++ src/main/java/com/F5/ALSports/dto/CategoryDTO.java
@@ -0,0 +1,6 @@
+package com.F5.ALSports.dto;
+
+public record CategoryDTO(
+        String name
+) {
+}
--- src/main/java/com/F5/ALSports/model/Category.java
+++ src/main/java/com/F5/ALSports/model/Category.java
@@ -12,6 +12,7 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(nullable = false)
     private String name;
     //private long ProductId;
 
