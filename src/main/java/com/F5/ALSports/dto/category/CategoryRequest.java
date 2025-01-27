83b689649b588716e4160333cfc0fb15b3a4dbfc
AngelB77 <AngelB1977@gamil.com>
2025-01-27 14:21:42 +0100

    Name-is-required

--- src/main/java/com/F5/ALSports/dto/category/CategoryRequest.java
+++ src/main/java/com/F5/ALSports/dto/category/CategoryRequest.java
@@ -2,5 +2,5 @@ package com.F5.ALSports.dto.category;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record CategoryRequest(@NotBlank(message = "Description is required") String name) {
+public record CategoryRequest(@NotBlank(message = "Name is required") String name) {
 }
