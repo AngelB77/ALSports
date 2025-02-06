1dcfd7e3b2c312befc50254b2eb45b71d7a9d242
AngelB77 <angelb1977@gmail.com>
2025-02-06 12:27:54 +0100

    User Mapper

--- src/main/java/com/F5/ALSports/dto/user/UserMapper.java
+++ src/main/java/com/F5/ALSports/dto/user/UserMapper.java
@@ -5,7 +5,7 @@ import com.F5.ALSports.model.User;
 
 public class UserMapper {
 
-   /* public static User dtoToEntity(UserRequest userRequest) {
+    public static User dtoToEntity(UserRequest userRequest) {
         return new User(userRequest.name(), userRequest.email(),
                 userRequest.password());
     }
@@ -13,7 +13,7 @@ public class UserMapper {
     public static UserResponse entityTodto(User user) {
         return new UserResponse(user.getName(), user.getEmail(),
                 user.getPassword());
-    }*/
+    }
 
 }
 
